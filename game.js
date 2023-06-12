dd4c6b4730eee3773a6107b95338c1fe62df99e4
Ritik Raj <raj769417@gmail.com>
2023-06-12 21:43:51 +0530

    added timer

--- game.js
+++ game.js
@@ -26,19 +26,19 @@ let score = 12;
 
 keys.forEach((key) => {
     key.addEventListener('click', (e) => {
-        
+
         const letter = e.target.innerText;
 
-        if(paused) return;
+        if (paused) return;
 
-        if(letter === '←') {
+        if (letter === '←') {
             eraseLetter();
 
             return;
         }
 
-        if(letter == 'Enter') {
-            if(guessWord.length !== 4) return;
+        if (letter == 'Enter') {
+            if (guessWord.length !== 4) return;
 
             changeBoxColor();
             checkWin();
@@ -51,15 +51,13 @@ keys.forEach((key) => {
 })
 
 document.addEventListener('keyup', (event) => {
-    
-    if(paused) return;
 
-    if(event.key == 'Backspace'){
-        eraseLetter();
-    }
+    if (paused) return;
 
-    if(event.key === 'Enter') {
-        if(guessWord.length !== 4) return;
+    if (event.key == 'Backspace') eraseLetter();
+
+    if (event.key === 'Enter') {
+        if (guessWord.length !== 4) return;
 
         changeBoxColor();
         checkWin();
@@ -76,14 +74,15 @@ document.addEventListener('keyup', (event) => {
         // score -= 2;
     }
 
-    
-    if(event.key.match('[A-Za-z]')===null || event.key.length!==1) {
+
+    if (event.key.match('[A-Za-z]') === null || event.key.length !== 1) {
         return;
     }
 
     insertLetter(event.key.toUpperCase());
 })
 
+// get current active box where letter need to be inserted
 function getActiveBox(box, index) {
     return box.classList.contains('selected');
 }
@@ -102,25 +101,26 @@ function eraseLetter() {
 function changeBoxColor() {
     const currentActiveIndex = boxes.findIndex(getActiveBox) + 1;
     const previousBoxes = [...boxes].slice((guessCount * 4) - 4, currentActiveIndex);
-    
+
     previousBoxes.forEach((box, ind) => {
-        console.log(box.innerText,'    ', word[ind]);
-        if(box.innerText === word[ind]) {
+        console.log(box.innerText, '    ', word[ind]);
+        if (box.innerText === word[ind]) {
             box.style.color = 'white';
             box.style.backgroundColor = 'green';
 
+            // changing color of key elements
             keys.forEach((k) => {
-                if(k.innerText === word[ind]) {
+                if (k.innerText === word[ind]) {
                     k.style.backgroundColor = 'green';
                 }
             })
 
-        } else if(word.includes(box.innerText)) {
+        } else if (word.includes(box.innerText)) {
             box.style.color = 'white';
             box.style.backgroundColor = '#d4bc22';
 
             keys.forEach((k) => {
-                if(box.innerText === k.innerText) {
+                if (box.innerText === k.innerText) {
                     k.style.backgroundColor = '#d4bc22';
                 }
             })
@@ -129,34 +129,31 @@ function changeBoxColor() {
             box.style.backgroundColor = '#292929';
 
             keys.forEach((k) => {
-                if(box.innerText === k.innerText) {
+                if (box.innerText === k.innerText) {
                     k.style.backgroundColor = '#292929';
                 }
             })
-            
+
         }
     })
 }
 
 function checkWin() {
-    if(guessWord === word) {
+    if (guessWord === word) {
         console.log('You Won !!!');
 
-        // setTimeout(() => {
-        //     location.reload();
-        // }, 1000);
+        clearInterval(interval);
 
         paused = true
         showPopup('You Won !!!', `Score : ${score}`);
-        
+
         return true;
     }
-    if(guessCount == 6) {
+
+    if (guessCount == 6) {
         console.log('You lost :(');
 
-        // setTimeout(() => {
-        //     location.reload();
-        // }, 1000);
+        clearInterval(interval);
 
         paused = true;
         showPopup('You Lost :(', `The correct word was ${word}`);
@@ -173,28 +170,27 @@ function checkWin() {
     guessCount += 1;
 
     score -= 2;
-    
 }
 
 function insertLetter(key) {
     const pos = boxes.findIndex(getActiveBox);
     const activeBox = boxes[pos];
-    
+
     activeBox.innerText = key;
 
-    if(guessWord.length === word.length) {
-        guessWord = guessWord.substring(0, guessWord.length-1);
+    if (guessWord.length === word.length) {
+        guessWord = guessWord.substring(0, guessWord.length - 1);
     }
 
     guessWord += key;
-   
-    if(guessWord.length !== word.length)
-    {
+
+    if (guessWord.length !== word.length) {
         activeBox.classList.remove('selected');
-        boxes[pos+1].classList.add('selected');
+        boxes[pos + 1].classList.add('selected');
     }
 }
 
+// show popup on win or loss
 
 function showPopup(title, message) {
     popup.querySelector('h2').innerText = title;
@@ -203,8 +199,40 @@ function showPopup(title, message) {
     popup.style.transform = 'scale(1)';
 }
 
+
+// reset the game
+
 document.querySelector('button.play').addEventListener('click', (e) => {
     location.reload();
 })
 
+// start timer
+
+let countdown = 59;
+let interval = null;
+
+const startTimer = () => {
+    const timer = document.querySelector('.timer');
+    const timer_val = timer.querySelector('.circle');
+
+    
+
+    interval = setInterval(() => {
+        timer.style.background = `conic-gradient(rgb(236, 181, 15) ${(60 - countdown) * 6}deg, rgb(47, 45, 45) 0deg)`;
+
+        timer_val.innerText = `0:${countdown}`;
+        countdown--;
+
+
+        if (countdown < 0) {
+
+            showPopup('Times up !!!', `The correct word was ${word}`);
+
+            timer_val.innerText = `0:00`;
+            clearInterval(interval);
+        }
+    }, 1000)
+}
+
+startTimer();
 
